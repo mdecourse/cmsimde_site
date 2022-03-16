6fd9642a0d540a4b209419f6ab6522ca4f0ebad7
40923113 <40923113@gm.nfu.edu.tw>
2022-03-16 10:03:35 +0800

    test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n test1234', 'tags': '', 'url': 'About.html'}]};
